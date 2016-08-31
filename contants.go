f06286715846c4676343d75681910efeaaf23cdb
Shravan Kumar Gond <shravan.ma.iitkgp@gmail.com>
2016-08-31 16:15:18 +0530

    Loop

--- contants.go
+++ contants.go
@@ -7,7 +7,7 @@ const Pi = 3.14
 func main() {
 	const World = "世界"
 	fmt.Println("Hello", World)
-	fmt.Println("Happy", Pi, "Day")
+	fmgit at.Println("Happy", Pi, "Day")
 
 	const Truth = true
 	fmt.Println("Go rules?", Truth)
